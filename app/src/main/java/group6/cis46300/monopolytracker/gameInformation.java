2934238e9ed9006a97c964704fbc1e22b599c467
Drew <drew.faber.89@gmail.com>
2015-04-25 17:25:13 -0500

    -Changed the layout of the two listviews slightly so they dont overlap on a phone view -changed intial values for Comm. chest and banker -added pay statement to track the users current progression of paying money -added pop up alertdialog to allow users to enter amount to be payed to other user -added complete transaction button that need to coded still, I need to take a break might finish it tonight that listener will be responsible for moving the funds and resetting the information for the next transaction.

--- app/src/main/java/group6/cis46300/monopolytracker/gameInformation.java
+++ app/src/main/java/group6/cis46300/monopolytracker/gameInformation.java
@@ -1,15 +1,25 @@
 package group6.cis46300.monopolytracker;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.method.DigitsKeyListener;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,6 +27,10 @@ import android.widget.TextView;
 public class gameInformation extends ActionBarActivity {
     ListView listView ;
     ListView moneyView;
+    Object payer, payee;
+    String currentPayer, payStatementOutput, amount;
+    boolean needPayer = true, needPayee = true;
+    Button pay;
 
 
     @Override
@@ -24,40 +38,116 @@ public class gameInformation extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_information);
 
+        final TextView payStatement = (TextView)findViewById(R.id.textView2);
         listView = (ListView) findViewById(R.id.list);
         moneyView = (ListView) findViewById(R.id.listView);
+        Button payBtn = (Button) findViewById(R.id.button);
 
         Intent intent = getIntent();
         String[] myStrings = intent.getStringArrayExtra("strings");
 
-       final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.textView, myStrings);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.textView, myStrings);
         listView.setAdapter(adapter);
 
         Integer[] moneyTracker = new Integer[myStrings.length];
 
-        for(int i = 0; i < moneyTracker.length; i++)
+        for(int i = 0; i < moneyTracker.length-1; i++)
         {
             moneyTracker[i] = 1500;
         }
+        //set values for Comm.Chest and Banker
+        moneyTracker[moneyTracker.length-2] = 0;
+        moneyTracker[moneyTracker.length-1] = 50000;
 
        ArrayAdapter<Integer> moneyAdapter = new ArrayAdapter<Integer>(this, R.layout.list_item, R.id.textView, moneyTracker);
        moneyView.setAdapter(moneyAdapter);
 
         //set onclick listeners
-        //This is to select the payer, which will be highlighted.
+        //This is to select the payer, which will be highlighted. Payer = cyan  and payee = yellow
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-               // adapter.notifyDataSetChanged();
-                for(int i = 0; i < listView.getChildCount(); i++)
-                {
-                    listView.getChildAt(i).setBackgroundColor(Color.WHITE);
+            //checking to see if this is payer or payee
+                if (needPayer) {
+                    // adapter.notifyDataSetChanged();
+                    /*
+                    for (int i = 0; i < listView.getChildCount(); i++) {
+                        listView.getChildAt(i).setBackgroundColor(Color.WHITE);
+                        moneyView.getChildAt(i).setBackgroundColor(Color.WHITE);
+                    } */
+
+                    //set payer so we can transfer money though array later
+                    payer = listView.getItemAtPosition(position);
+
+                    //updating paystatement with payer
+                    payStatementOutput = listView.getItemAtPosition(position).toString();
+                    payStatement.setText(payStatementOutput);
+
+                    //updating background of payer to cyan
+                    listView.getChildAt(position).setBackgroundColor(Color.CYAN);
+                    moneyView.getChildAt(position).setBackgroundColor(Color.CYAN);
+
+                }else{
+                    /*
+                    for (int i = 0; i < listView.getChildCount(); i++) {
+                        listView.getChildAt(i).setBackgroundColor(Color.YELLOW);
+                        moneyView.getChildAt(i).setBackgroundColor(Color.YELLOW);
+                    } */
+
+                    //changing payee background to yellow
+                    listView.getChildAt(position).setBackgroundColor(Color.YELLOW);
+                    moneyView.getChildAt(position).setBackgroundColor(Color.YELLOW);
+
+                    //set payee to move funds later
+                    payee = listView.getItemAtPosition(position);
+
+                    //updating paystatement with payee
+                    payStatementOutput = payStatementOutput+listView.getItemAtPosition(position).toString();
+                    payStatement.setText(payStatementOutput);
+
+                    //creating pop up input text for user
+                    final EditText input = new EditText(gameInformation.this);
+                    // set input keyboard to numerical only
+                    input.setKeyListener(DigitsKeyListener.getInstance());
+                    new AlertDialog.Builder(gameInformation.this)
+                            .setTitle("Enter the amount owed.")
+                            .setMessage(payStatementOutput)
+                            .setView(input)
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    //set amount to be transfered
+                                     amount = input.getText().toString();
+                                    //update payStatement
+                                     payStatementOutput = payStatementOutput+" $"+amount;
+                                     payStatement.setText(payStatementOutput);
+                                }
+                            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            // Do nothing.
+                            //if user clicked cancel should probably clear paystatement and set isPayer = 0 to completely restart process
+                        }
+                    }).show();
+
+                    //need to put button listener for complete transaction
+                    //this is where we will change the funds between the users and reset the boolean to true and the paystatement to blank
                 }
-                listView.getChildAt(position).setBackgroundColor(Color.CYAN);
             }
         });
 
+        payBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //set isPayer to false so we know that we don't need them to select a payer
+                //maybe add a way to check and make sure that a payer is set like if(payer.getText().toString().trim().length()) != 0)
+                //if nothing is selected send the back to make another selection
+                needPayer = false;
+                payStatementOutput = payStatementOutput+" will pay ";
+                payStatement.setText(payStatementOutput);
+            }
+        });
+
+
+/*
         moneyView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -70,7 +160,7 @@ public class gameInformation extends ActionBarActivity {
                 moneyView.getChildAt(position).setBackgroundColor(Color.YELLOW);
             }
         });
-
+*/
 
 
 //hello how u iz?
@@ -83,7 +173,6 @@ public class gameInformation extends ActionBarActivity {
 // values[0] = "Test2";
 // adapter.notifyDataSetChanged();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
